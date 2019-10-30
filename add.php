b9d7f4fa491f72393c02778cac21cc54dd7ede22
Yeddu Naresh Kumar <nareshy@VALUEMOMENTUM.BIZ>
2019-10-30 14:24:39 +0530

    Exception handled in add

--- add.php
+++ add.php
@@ -9,7 +9,7 @@
 include_once("config.php");
  
 if(isset($_POST['Submit'])) {   
-    if (isset($_FILES["cover"])) {
+    if (isset($_FILES["cover"]) && !empty($questionCover["tmp_name"])) {
         $questionCover = $_FILES["cover"];
         $user["cover"] =  new MongoDB\BSON\Binary(file_get_contents($questionCover["tmp_name"]), MongoDB\BSON\Binary::TYPE_GENERIC);
     }
@@ -31,11 +31,20 @@ if(isset($_POST['Submit'])) {
         echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";    
     } else {
         //insert data to database table/collection named 'users'
-        $db->insertOne($user);
-        
-        //display success message
-        echo "<font color='green'>Data added successfully.";
-        echo "<br/><a href='index.php'>View Result</a>";
+        try{
+            $db->insertOne($user);
+
+            //display success message
+            echo "<font color='green'>Data added successfully.";
+            echo "<br/><a href='index.php'>View Result</a>";
+        } catch(MongoDB\Driver\Exception\BulkWriteException $e) {
+            $filename = basename(__FILE__);
+            echo "The $filename script has experienced an error.<br/>"; 
+            echo "It failed with the following exception:<br/>";
+            echo "Exception:", $e->getMessage(), "<br/>";
+            echo "In file:", $e->getFile(), "<br/>";
+            echo "On line:", $e->getLine(), "<br/>";    
+        }
     }
 }
 ?>
